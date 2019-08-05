3264e6aaf626a58d30b62469ebf781d3702a4700
r.vanderaspoilden <r.vanderspoilden@kiabi.com>
2019-08-05 14:17:23 +0200

    :sparkles: : join room + disconnect

---
+++ src/models/room-status.ts
@@ -0,0 +1,5 @@
+export enum RoomStatus {
+    OPEN = 'OPEN',
+    FULL = 'FULL',
+    INGAME = 'INGAME'
+}
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,11 +1,12 @@
 import {Vector3} from "./vector3";
 
 export class User {
-    constructor(public uid: string,
-                public username: string,
-                public champion: string,
-                public position: Vector3,
-                public rotation: Vector3) {
-        
+    public uid: string;
+    public username: string;
+    public champion: string;
+    public position: Vector3;
+    public rotation: Vector3;
+
+    constructor() {
     }
 }
